4b73a63077a74875bb036381fdcaf41a416ed601
Faris Luthfan R <farislr@faris-mbp.local>
2025-09-03 19:40:10 +0700

    update deps
    add color scheme snippet

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,4 +2,25 @@ return {
   "folke/tokyonight.nvim",
   lazy = true,
   opts = { style = "moon" },
+
+  -- "catppuccin/nvim",
+  -- lazy = true,
+  -- opts = {},
+  --
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "catppuccin-macchiato",
+  --   },
+  -- },
+  --
+  -- {
+  --   "akinsho/bufferline.nvim",
+  --   init = function()
+  --     local bufline = require("catppuccin.groups.integrations.bufferline")
+  --     function bufline.get()
+  --       return bufline.get_theme()
+  --     end
+  --   end,
+  -- },
 }
