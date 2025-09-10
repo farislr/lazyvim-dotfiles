b10902fd40912f95706e41e0e0a592a0192ddd70
Faris Luthfan R <farislr@faris-mbp.local>
2025-09-10 15:08:55 +0700

    chore: rename neotest config

--- lua/plugins/claude.lua
+++ lua/plugins/claude.lua
@@ -1,7 +1,7 @@
-return {
-  "greggh/claude-code.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim", -- Required for git operations
-  },
-  opts = {},
-}
+-- return {
+--   "greggh/claude-code.nvim",
+--   dependencies = {
+--     "nvim-lua/plenary.nvim", -- Required for git operations
+--   },
+--   opts = {},
+-- }
