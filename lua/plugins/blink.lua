73df57bef9de37589d2405ec44b8360bc18ec0e6
Faris Luthfan Ramadhan <farislr@faris-mbp.local>
2026-01-12 19:16:12 +0700

    feat: WIP blink copilot integration

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,11 +1,28 @@
 return {
   "saghen/blink.cmp",
+  dependencies = {
+    { "giuxtaposition/blink-cmp-copilot" },
+  },
   opts = {
-    completion = { list = { selection = { preselect = false, auto_insert = true } } },
-    keymap = {
-      preset = "default",
-      ["<Tab>"] = { "select_next" },
-      ["<S-Tab>"] = { "select_prev" },
+    sources = {
+
+      default = { "lsp", "path", "snippets", "buffer", "copilot" },
+      { name = "lsp", priority = 10 },
+      { name = "copilot", priority = 5 },
+      providers = {
+        copilot = {
+          name = "copilot",
+          module = "blink-cmp-copilot",
+          score_offset = 100,
+          async = true,
+        },
+      },
     },
+    -- completion = { list = { selection = { preselect = false, auto_insert = true } } },
+    -- keymap = {
+    --   preset = "default",
+    --   ["<Tab>"] = { "select_next" },
+    --   ["<S-Tab>"] = { "select_prev" },
+    -- },
   },
 }
---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,11 @@
+return {
+  "zbirenbaum/copilot.lua",
+  opt = {
+    suggestion = {
+      virtual_text = false,
+    },
+    panel = {
+      enabled = false,
+    },
+  },
+}
