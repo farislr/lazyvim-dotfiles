0ce87aa6cca35c6148dcfcebc5dd51abe4a7e75f
Faris Luthfan R <LRFaris24@gmail.com>
2026-03-31 07:32:52 +0700

    chore: remove blink config

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,28 +1,28 @@
 return {
-  "saghen/blink.cmp",
-  dependencies = {
-    { "giuxtaposition/blink-cmp-copilot" },
-  },
-  opts = {
-    sources = {
-
-      default = { "lsp", "path", "snippets", "buffer", "copilot" },
-      { name = "lsp", priority = 10 },
-      { name = "copilot", priority = 5 },
-      providers = {
-        copilot = {
-          name = "copilot",
-          module = "blink-cmp-copilot",
-          score_offset = 100,
-          async = true,
-        },
-      },
-    },
-    -- completion = { list = { selection = { preselect = false, auto_insert = true } } },
-    -- keymap = {
-    --   preset = "default",
-    --   ["<Tab>"] = { "select_next" },
-    --   ["<S-Tab>"] = { "select_prev" },
-    -- },
-  },
+  -- "saghen/blink.cmp",
+  -- dependencies = {
+  --   { "giuxtaposition/blink-cmp-copilot" },
+  -- },
+  -- opts = {
+  --   sources = {
+  --
+  --     default = { "lsp", "path", "snippets", "buffer", "copilot" },
+  --     { name = "lsp", priority = 10 },
+  --     { name = "copilot", priority = 5 },
+  --     providers = {
+  --       copilot = {
+  --         name = "copilot",
+  --         module = "blink-cmp-copilot",
+  --         score_offset = 100,
+  --         async = true,
+  --       },
+  --     },
+  --   },
+  -- completion = { list = { selection = { preselect = false, auto_insert = true } } },
+  -- keymap = {
+  --   preset = "default",
+  --   ["<Tab>"] = { "select_next" },
+  --   ["<S-Tab>"] = { "select_prev" },
+  -- },
+  -- },
 }
