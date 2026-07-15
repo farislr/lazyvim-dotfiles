b5ced89eedc9e9fa830a7331a07d77a7b60829f8
Faris Luthfan R <LRFaris24@gmail.com>
2026-07-15 10:35:57 +0700

    feat: add helm ls

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -3,7 +3,7 @@ return {
   opts = {
     servers = {
       qmlls = {
-        cmd = { "/home/linuxbrew/.linuxbrew/bin/qmlls", "-E" },
+        cmd = { "/usr/bin/qmlls6", "-E" },
       },
       gopls = {
         settings = {
@@ -12,6 +12,34 @@ return {
           },
         },
       },
+      -- helm_ls = {
+      --   logLevel = "info",
+      --   valuesFiles = {
+      --     mainValuesFile = "values.yaml",
+      --     lintOverlayValuesFile = "values.lint.yaml",
+      --     additionalValuesFilesGlobPattern = "values*.yaml",
+      --   },
+      --   helmLint = {
+      --     enabled = true,
+      --     ignoredMessages = {},
+      --   },
+      --   yamlls = {
+      --     enabled = false,
+      --     enabledForFilesGlob = "*.{yaml,yml}",
+      --     diagnosticsLimit = 50,
+      --     showDiagnosticsDirectly = false,
+      --     path = "yaml-language-server", -- or something like { "node", "yaml-language-server.js" }
+      --     initTimeoutSeconds = 3,
+      --     config = {
+      --       schemas = {
+      --         kubernetes = "templates/**",
+      --       },
+      --       completion = true,
+      --       hover = true,
+      --       -- any other config from https://github.com/redhat-developer/yaml-language-server#language-server-settings
+      --     },
+      --   },
+      -- },
     },
   },
 }
